34078000af792bd4c78806f1d58c7568043c6f47
AKKi-28 <akshaymailforwork@gmail.com>
2024-03-13 22:21:53 +0530

    nav,footer,home,about - made responsive

--- components/About/About.jsx
+++ components/About/About.jsx
@@ -17,16 +17,16 @@ const About = () => {
         <div className='myname'>
           A K S H A Y  &nbsp; N S
         </div>
-        <div className="container">
+        
           <div className="profile">
             Hello! I'm Akshay NS, a 23-year-old Computer Science Engineering graduate passionate about Software Development. As a fresher, I'm eagerly seeking opportunities to contribute to innovative projects and teams. If you're interested in discussing opportunities, please feel free to contact me through the contact section.
           </div>
-          <div className="pic">
+          
             <div className="img-container">
               <img src="Images\pic.png" alt="My Profic Picture" />
             </div>
-          </div>
-        </div>
+          
+        
         <div className="resume">
           <button className="button" onClick={openPdfFile}><b>Resume</b></button>
         </div>
